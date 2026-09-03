b3822a3cb61afd5744b21d5cdc6914bddac82580
Dilkash Gorka <gorkadilkash91@gmail.com>
2026-09-03 07:43:26 +0530

    Time: 4 ms (42.06%), Space: 30.3 MB (38.75%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.cpp
@@ -1,12 +1,26 @@
 class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
-        for (int i = 0; i < nums.size(); i++) {
-            nums[i] = nums[i] * nums[i];   // har number ko usi jagah square kar do
+
+        // we can also do it wioth this in optimal soln with two pointer apporooach as with sorting it was nlogn 
+        int left = 0;
+        int right = nums.size() - 1; // indexing of two pointer 
+
+        vector<int> ans(nums.size()); // creating ans array 
+
+        for (int i = nums.size() - 1; i >= 0; i--) { 
+            // for loop will start to work from last ans check the possibilities written down 
+
+            if (abs(nums[left]) > abs(nums[right])) {
+                ans[i] = nums[left] * nums[left]; // squaring
+                left++;  // abs is used so that we can get -3>-1 as we know 9>1 or -2>-1 as 4>1 thats why we used abs for this 
+            }
+            else {
+                ans[i] = nums[right] * nums[right];
+                right--;
+            }
         }
-        
-        sort(nums.begin(), nums.end());    // fir sort kar do
-        
-        return nums;
+
+        return ans;
     }
 };
