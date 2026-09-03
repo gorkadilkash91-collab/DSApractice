d55a0dfe72093720e36078598cf7349cfc55c97e
Dilkash Gorka <gorkadilkash91@gmail.com>
2026-09-03 14:07:45 +0530

    Time: 0 ms (100%), Space: 14 MB (50.56%) - LeetHub

--- 1089-duplicate-zeros/1089-duplicate-zeros.cpp
+++ 1089-duplicate-zeros/1089-duplicate-zeros.cpp
@@ -1,15 +1,29 @@
 class Solution {
 public:
     void duplicateZeros(vector<int>& arr) {
-         int i;
-        for(i =0; i<arr.size(); i++){
-            if(arr[i]==0){
-                arr.insert(arr.begin()+i+1,0);
-                arr.pop_back();
-                i++;
+        int duplicates = 0;
+        int lastindex = arr.size() - 1;
+        for (int i = 0; i <= lastindex - duplicates; i++) {
+            if (arr[i ] == 0) {
+                // edge case
+                if (i == lastindex - duplicates) {
+                    arr[lastindex] = 0;
+                    lastindex-=1;
+                    break;
+                }
+                duplicates++;
             }
         }
-        
-        
+        int nayalastindex = lastindex - duplicates;
+        for( int i =nayalastindex ; i>=0; i--){
+            if(arr[i]==0){
+                arr[i+duplicates]=0;
+                duplicates--;
+                arr[i+duplicates]=0;
+            }
+            else{
+                arr[i+duplicates]= arr[i];
+            }
+        }
     }
 };
