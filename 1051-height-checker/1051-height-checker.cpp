11709516daa7d71cb4856b98741442cd6bfd211a
Dilkash Gorka <gorkadilkash91@gmail.com>
2026-09-07 21:35:26 +0530

    Time: 0 ms (100%), Space: 11.3 MB (60.26%) - LeetHub

--- 1051-height-checker/1051-height-checker.cpp
+++ 1051-height-checker/1051-height-checker.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int heightChecker(vector<int>& heights) {
-        vector<int> expected = heights;
-        sort(heights.begin(), heights.end());
-        int count = 0;
-        for (int i = 0; i < heights.size(); i++) {
-            if (heights[i] != expected[i]) {
-                count++;
+        vector<int> expected = heights; // idka mtlb hai ki humne ekh heights  name ke vector ki copy bna kr uska name expected rakh dia hai ... 
+        sort(heights.begin(), heights.end()); // soprt krdia
+        int count = 0; /// check kia ki expected aur aur given vale match kri ki nhi sort krne ke lia eslia count ka variable lena pdha ekh 
+        for (int i = 0; i < heights.size(); i++) { // traverse krdia 
+            if (heights[i] != expected[i]) { // iske bina case run nhi krega 
+                count++; // oh yes 
             }
         }
         return count;
